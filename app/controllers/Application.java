78e65d46f8d99865edf788575312aebfa08415d7
Denis Migranov <d.migranov@g.nsu.ru>
2019-04-14 18:29:08 +0700

    most popular troubles done

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -28,9 +28,16 @@ public class Application extends Controller {
 
     public static void troubles() {
 
-        List troubles = Project.find(
+        /*List troubles = Project.find(
                 "select t.name, trouble_count FROM Trouble t JOIN (select t.id as id, count(*) as trouble_count FROM Project p JOIN Trouble t ON p.trouble.id = t.id GROUP BY t.id) as tc ON t.id = tc.id"
+        ).fetch();*/
+
+        List troubles = Project.find(
+                "select t.name, count(*) as trouble_count FROM Project p JOIN Trouble t ON p.trouble = t GROUP BY t.id, t.name"
         ).fetch();
+
+        troubles.get(0);
+
 
         //System.out.println(troubles.get(0).count);
 
