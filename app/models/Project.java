f5e5b142a89a2e4ddcb9e65775b172b392ff41d7
Denis Migranov <d.migranov@g.nsu.ru>
2019-04-14 14:26:12 +0700

    added project model

---
+++ app/models/Project.java
@@ -0,0 +1,30 @@
+package models;
+
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+public class Project {
+    @ManyToOne
+    Client client;
+
+    @ManyToOne
+    Employee engineer;
+
+    @ManyToOne
+    Employee manager;
+
+    long detailCost; //нужно ли хранить? можно же посчитаьт
+    int workCost;
+
+    Date workBegin, workEnd;
+
+    boolean isGuaranteed;
+
+    @ManyToOne
+    Trouble trouble;    //или лучше manytomany?
+
+    boolean type;
+
+
+}
