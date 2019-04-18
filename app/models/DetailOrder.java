80e1d9af602b8f172e615a7cd18c32f6f10b0dd2
Denis Migranov <d.migranov@g.nsu.ru>
2019-04-18 20:52:41 +0700

    detailorder

--- app/models/DetailOrder.java
+++ app/models/DetailOrder.java
@@ -3,13 +3,19 @@ package models;
 import play.data.validation.Min;
 import play.db.jpa.Model;
 
+import javax.persistence.ManyToOne;
+
 public class DetailOrder extends Model {
+    @ManyToOne
     public Project project;
+    @ManyToOne
     public Detail detail;
-    @Min(0)int count;   //trigger!
+    @Min(0) public int count;   //trigger!
 
-    public DetailOrder()
+    public DetailOrder(Project project, Detail detail, int count)
     {
-
+        this.project = project;
+        this.detail = detail;
+        this.count = count;
     }
 }
