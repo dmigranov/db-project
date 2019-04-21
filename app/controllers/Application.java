610414f50789eafd73bc34a68dc5c8ec761e2335
Denis Migranov <d.migranov@g.nsu.ru>
2019-04-21 19:28:54 +0700

    actual

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -114,10 +114,9 @@ public class Application extends Controller {
         render(projects);
     }
 
-    //id - detailType.id НЕ готово!!!!
+    //id - detailType.id
     public static void popularDetails(long id) {
-        Detail.find("Select d FROM Detail d JOIN DetailOrder o ON DetailOrder.detail = d WHERE d.type = ?1 GROUP BY d.id", id).fetch(5);
-
+        Detail.find("Select count(d.id) FROM Detail d JOIN DetailOrder o ON DetailOrder.detail = d WHERE d.type = ?1 GROUP BY d.id", id).fetch(5);
         render();
     }
 
